7ab7314f303c3c813b2d015f49e2767f0d128c95
Alejandro314 <alejandro_roman_caballero@outlook.es>
2019-03-04 00:11:27 +0100

    Ya funcionan ambos contenedores el problema es que no puedo conectarme con mysql a la cuenta root con mysql -u root -p

--- GBD/inicioservicio.sh
+++ GBD/inicioservicio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+service mysql start
 sed -i 's/root/'"$user_1"'/g' /usr/bin/carga.sql
 sed -i 's/condoriano/'"$user_2"'/g' /usr/bin/carga.sql
 sed -i 's/localhost/'"$host"'/g' /usr/bin/carga.sql
@@ -10,6 +10,6 @@ sed -i 's/alebuntu/'"$gbd"'/g' /usr/bin/carga.sql
 
 
 
-mysql -u root -pcontrasena < /usr/bin/carga.sql
+#mysql -u root -pcontrasena < /usr/bin/carga.sql
 
 
